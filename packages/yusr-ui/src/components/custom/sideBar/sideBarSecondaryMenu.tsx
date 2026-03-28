0e83add1bfb8c2e1ffc090bea9b286f18cf65b6d
Ahmed Bakri <AhmedBkry204@gmail.com>
2026-03-28 12:42:54 +0300

    enhance- sidebar secondary, make it generic and next.js safe

--- packages/yusr-ui/src/components/custom/sideBar/sideBarSecondaryMenu.tsx
+++ packages/yusr-ui/src/components/custom/sideBar/sideBarSecondaryMenu.tsx
@@ -1,42 +1,58 @@
 import { LogOutIcon } from "lucide-react";
 import * as React from "react";
-import { Link } from "react-router-dom";
-import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../../pure/sidebar";
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../../pure/sidebar";
 import { ThemeToggle } from "../theme/themeToggle";
 
-export interface SideBarSecondaryMenuProps extends React.ComponentPropsWithoutRef<typeof SidebarGroup> {
+export interface SideBarSecondaryMenuProps
+  extends React.ComponentPropsWithoutRef<typeof SidebarGroup> {
   items: {
     title: string;
     url: string;
     icon: React.ReactNode;
   }[];
   onLogout: () => void;
+  LinkComponent?: React.ElementType;
 }
 
-export function SideBarSecondaryMenu({ items, onLogout, ...props }: SideBarSecondaryMenuProps)
-{
+export function SideBarSecondaryMenu({
+  items,
+  onLogout,
+  LinkComponent = "a",
+  ...props
+}: SideBarSecondaryMenuProps) {
   return (
-    <SidebarGroup { ...props }>
+    <SidebarGroup {...props}>
       <SidebarGroupContent>
         <SidebarMenu>
-          { items.map((item) => (
-            <SidebarMenuItem key={ item.title }>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild>
-                <Link to={ item.url } className="flex items-center justify-start gap-3 w-full px-3">
-                  <span className="flex items-center justify-center shrink-0 size-4">{ item.icon }</span>
+                <LinkComponent
+                  href={item.url}
+                  className="flex items-center justify-start gap-3 w-full px-3"
+                >
+                  <span className="flex items-center justify-center shrink-0 size-4">
+                    {item.icon}
+                  </span>
 
-                  <span className="font-medium truncate">{ item.title }</span>
-                </Link>
+                  <span className="font-medium truncate">{item.title}</span>
+                </LinkComponent>
               </SidebarMenuButton>
             </SidebarMenuItem>
-          )) }
+          ))}
 
           <SidebarMenuItem>
             <ThemeToggle variant="sidebar" />
           </SidebarMenuItem>
 
           <SidebarMenuItem key="logout">
-            <SidebarMenuButton asChild onClick={ onLogout }>
+            <SidebarMenuButton asChild onClick={onLogout}>
               <div className="flex items-center justify-start gap-3 w-full px-3 text-destructive cursor-pointer">
                 <span className="flex items-center justify-center shrink-0 size-4">
                   <LogOutIcon />
